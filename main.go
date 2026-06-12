73927ceea24d04ab7ada4a67aec316c6a1146d14
Dylan <165859680+Dylan-302@users.noreply.github.com>
2026-06-12 21:03:10 +0000

    feat:db-connection-to-the-db

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"log"
+
+	db "banc-api/src/infrastructure/db/adapter"
+
+	"github.com/gofiber/fiber/v3"
+)
+
 func main() {
 
+	db.DBconection()
+
+	app := fiber.New()
+
+	app.Get("/", func(c fiber.Ctx) error {
+		return c.SendString("hello world")
+	})
+
+	if err := app.Listen(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
